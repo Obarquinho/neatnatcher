1cdd2fb25e4644f6a5dd2273e53c86277dc0d5ca
Obarquinho <pigsvanny@gmail.com>
2022-05-13 02:38:26 -0700

    API url changes

--- neatapi/neatapi/urls.py
+++ neatapi/neatapi/urls.py
@@ -15,8 +15,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework import routers
+from neatapi.nabberapi import views
 
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'countries', views.CountryViewSet)
+router.register(r'posts', views.PostViewSet)
+router.register(r'comments', views.CommentViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls'))
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls'), namespace='rest_framework')
 ]
